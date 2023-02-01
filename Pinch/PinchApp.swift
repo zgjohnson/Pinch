450f93065e0cf2bdbc907d5e9d9192acb791b421
Zach Johnson <zgjohnson94@gmail.com>
2023-01-31 18:59:52 -0600

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Zach Johnson on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
